b0cde805025f117fcd02a2178561d372c7842d70
Glory Praise <emmaglorypraise@gmail.com>
2021-12-23 08:29:31 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+const phrase = 'Web3Bridge';
+const encodedString = window.btoa( phrase ); 
+const decodedString = window.atob( encodedString );
+console.log("Hi, my name is Glory Praise and this is my solution")
+console.log("This is the encrypted phrase: ",encodedString)
+console.log("This is the decrypted phrase: ", decodedString)
